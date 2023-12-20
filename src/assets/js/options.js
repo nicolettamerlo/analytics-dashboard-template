4fb012d8ae095f01a93c43d19273f7736178d2c7
Nicoletta Merlo <nicolettamerlodev@gmail.com>
2023-12-20 14:14:01 +0100

    created dist folder

--- src/assets/js/options.js
+++
@@ -1,6 +0,0 @@
-import { slateGray, cloudyGray } from "./colors";
-if(location.href.includes("index.html")){
-    Chart.defaults.font.family = "'Montserrat', sans-serif";
-    Chart.defaults.color = slateGray;
-    Chart.defaults.borderColor = cloudyGray;    
-}
